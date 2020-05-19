ed5397c16a28f62628abad40b73d7d660e833bd7
Tushar <tushark3@gmail.com>
2020-05-19 21:32:50 +0530

    Updates

---
+++ precache-manifest.0abdacffc8244da2b309449928520757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fef0e37bcdc48724b8a88b1d2e0e730",
+    "url": "/ude/index.html"
+  },
+  {
+    "revision": "e52beccf1c723bb9d167",
+    "url": "/ude/static/css/main.d183383d.chunk.css"
+  },
+  {
+    "revision": "fac0cd099e9607e4e111",
+    "url": "/ude/static/js/2.2eb35954.chunk.js"
+  },
+  {
+    "revision": "ab6bd92c46b489bf0abe330868a5708e",
+    "url": "/ude/static/js/2.2eb35954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52beccf1c723bb9d167",
+    "url": "/ude/static/js/main.bc1aeb0a.chunk.js"
+  },
+  {
+    "revision": "25ccbf8a5168e494730b",
+    "url": "/ude/static/js/runtime-main.ca28bbed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ude/static/media/logo.5d5d9eef.svg"
+  }
+]);
